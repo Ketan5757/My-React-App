59c51e3e6a090f81fa1c037238e5af5e7310f253
Ketan <ketan.darekar99@gmail.com>
2026-01-10 10:32:06 +0100

    Create React App with bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Textarea from './components/Textarea';
+
+function App() {
+  return (
+    <>
+    <Navbar search ="search here"/>
+    <Textarea click ="click here to sort"/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Textarea.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+export default function Textarea(props) {
+    const[text,settext] = useState("");
+
+    const sortFunction=()=>{
+        let newtext= text.split("").sort().join("");
+        settext(newtext);
+    }
+    
+    const clearText=()=>{
+        settext("")
+    }
+
+    const handleonChange =(event)=>{
+        settext(event.target.value)
+    }
+
+  return (
+    <>
+    <textarea className="form-control" id="1" rows ="8" cols ="20" value ={text} onChange={handleonChange}></textarea>
+    <button className="btn btn-primary mx-2" onClick ={sortFunction}>{props.click}</button>
+    <button className="btn btn-primary mx-2" onClick ={clearText}>Clear Text</button>
+    
+    </>
+  )
+}

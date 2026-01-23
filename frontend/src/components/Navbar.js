1c981684f5888178183e4cfc650fb17619519175
Ketan <ketan.darekar99@gmail.com>
2026-01-23 10:31:22 +0100

    Navbar update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
@@ -12,7 +13,13 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <Link className="nav-link active" aria-current="page" to="/main">Main</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/odd">Odd</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/textarea">Textarea</Link>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/">Link</a>

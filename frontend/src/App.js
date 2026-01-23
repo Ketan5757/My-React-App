d9205a46c50376378aa4f098376685c3502a1e89
Ketan <ketan.darekar99@gmail.com>
2026-01-23 10:25:43 +0100

    React routing using router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,19 @@ import Main from './components/Main';
 import Navbar from './components/Navbar';
 import Odd from './components/Odd';
 import Textarea from './components/Textarea';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <Navbar search ="search here"/>
-    <Odd title = "Enter Your Information"/>
-    <Textarea click ="click here to sort" preview ="Text Preview"/>
-    <Main heading = "List of Fruits" title ="Fill out the form" submit= "Submit form" reverse="Reverse the text" length="Typed Text"/>
+    <BrowserRouter>
+    <Routes>
+      <Route path ="/" element ={<Navbar search ="search here"/>}></Route>
+      <Route path='/Odd' element={<Odd title = "Enter Your Information"/>}></Route>
+      <Route path='/textarea' element={<Textarea click ="click here to sort" preview ="Text Preview"/>}></Route>
+      <Route path='/main' element={<Main heading = "List of Fruits" title ="Fill out the form" submit= "Submit form" reverse="Reverse the text" length="Typed Text"/>}></Route>
+    </Routes>
+     </BrowserRouter>
     
     </>
   );
